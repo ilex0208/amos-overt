29e478308284cef1e065a6288a1dd7dbf5aeca18
ilex0208 <tiangonglei@126.com>
2016-11-13 22:23:53 +0800

    *)修改导入

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-var JTopo = require('./lib/jtl.js');// 0.4.8
-module.exports = JTopo;
+var _jtl = require('./lib/jtl.js');// 0.4.8
+module.exports = {
+    Element: _jtl.Element,
+    util: _jtl.util,
+    Stage: _jtl.Stage,
+    Scene: _jtl.Scene,
+    DisplayElement: _jtl.DisplayElement,
+    InteractiveElement: _jtl.InteractiveElement,
+    EditableElement: _jtl.EditableElement,
+    Node: _jtl.Node,
+    TextNode: _jtl.TextNode,
+    LinkNode: _jtl.LinkNode,
+    CircleNode: _jtl.CircleNode,
+    AnimateNode: _jtl.AnimateNode,
+    Link: _jtl.Link,
+    FoldLink: _jtl.FoldLink,
+    FlexionalLink: _jtl.FlexionalLink,
+    CurveLink: _jtl.CurveLink,
+    Container: _jtl.Container,
+    layout: _jtl.layout,
+    BarChartNode: _jtl.BarChartNode,
+    PieChartNode: _jtl.PieChartNode,
+    Animate: _jtl.Animate,
+    Effect: _jtl.Effect,
+    Logo: _jtl.Logo
+};
