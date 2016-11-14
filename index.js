139bb140d48bbfcf778b9c54c23add29d74f238f
ilex0208 <tiangonglei@126.com>
2016-11-14 23:57:54 +0800

    *）去除无用代码。

--- index.js
+++ index.js
@@ -1,29 +1,2 @@
-// var _jtl = require('./lib/jtl.js');// 0.4.8
-// module.exports = {
-//   Element: _jtl.Element,
-//   util: _jtl.util,
-//   Stage: _jtl.Stage,
-//   Scene: _jtl.Scene,
-//   DisplayElement: _jtl.DisplayElement,
-//   InteractiveElement: _jtl.InteractiveElement,
-//   EditableElement: _jtl.EditableElement,
-//   Node: _jtl.Node,
-//   TextNode: _jtl.TextNode,
-//   LinkNode: _jtl.LinkNode,
-//   CircleNode: _jtl.CircleNode,
-//   AnimateNode: _jtl.AnimateNode,
-//   Link: _jtl.Link,
-//   FoldLink: _jtl.FoldLink,
-//   FlexionalLink: _jtl.FlexionalLink,
-//   CurveLink: _jtl.CurveLink,
-//   Container: _jtl.Container,
-//   layout: _jtl.layout,
-//   BarChartNode: _jtl.BarChartNode,
-//   PieChartNode: _jtl.PieChartNode,
-//   Animate: _jtl.Animate,
-//   Effect: _jtl.Effect,
-//   Logo: _jtl.Logo
-// };
-
 var _at = require('./lib/at.js');// 0.4.8
 module.exports = _at;
